ace7a4f34ff45f20516ee505faad1dc108b20daa
tanimahossain <56477789+tanimahossain@users.noreply.github.com>
2020-09-08 16:10:11 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -5,7 +5,7 @@ vector <int> prime; //In this vector all the primes are saved
 bitset<MAXP> mark; //nonprime indexes are true, prime indexes are false here
 
 void sieve(){
-    int m=1.0*sqrt(MAXP)+2; int x;
+    int m=sqrt(MAXP); int x;
     prime.push_back(2); prime.push_back(3);
     mark[0]=mark[1]=true;
     for(int i=4;i<MAXP;i+=2)mark[i]=true;
@@ -13,13 +13,13 @@ void sieve(){
     for(int i=5;i<MAXP;i+=6){
         if(!mark[i]){
             prime.push_back(i);
-            if(i>m) continue;
-            for(int j=i*i;j<MAXP;j+=2*i)mark[j]=true;
+            if(i<=m) 
+                for(int j=i*i;j<MAXP;j+=2*i)mark[j]=true;
         }
         if(!mark[i+2]){ x=i+2;
             prime.push_back(x);
-            if(x>m) continue;
-            for(int j=x*x;j<MAXP;j+=2*x)mark[j]=true;
+            if(x<=m)
+                for(int j=x*x;j<MAXP;j+=2*x)mark[j]=true;
         }
     }
 }
